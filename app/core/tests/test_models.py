0c381e43539222deb115665245904fc1146f6f18
babuy45 <ashabibi49@gmail.com>
2020-05-18 06:53:29 +0700

    create user

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,38 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class modelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Melalui test akan dibuat user dengan email bila sukses"""
+        email = 'test@gmail.com'
+        password = 'testpassword123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test email untuk user baru yang di normalized"""
+        email = 'test@GMAIL>COM'
+        user = get_user_model().objects.create_user(email, 'test123')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test membuat user baru tanpa email akan error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_superuser(self):
+        """Test membuat superuser baru"""
+        user = get_user_model().objects.create_superuser(
+            'test@gmail.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
